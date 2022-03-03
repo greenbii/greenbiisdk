3ffb41ff4a8a0d997a57edbacce62cd05d64a6b1
Aniwura Tijani <teedon@gmail.com>
2022-03-03 17:55:32 +0100

    update the sdk_server

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const GB_API_SERVER = "http://localhost:8087/sdk/v1";  //"https://api.greenbii.com/v1"
+const GB_API_SERVER = "https://api.greenbii.com/v1"
 //const isBrowser = require('./check_browser');
 
 
